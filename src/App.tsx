ae9e83ffca0097ec47795dad3f4a99281c76e422
bazla <dobr1nya@mail.ru>
2021-12-17 11:48:59 +0300

    init: start build portfolio

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+import './index.scss';
+
+export const App = () => (
+  <Fragment>
+    <h1>React TypeScript Webpack Starter Template</h1>
+  </Fragment>
+);
+
+export default App;
